8fd0dd71c917815fd6278693344e9fa803e797c3
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-05 19:32:14 +0530

    Add Main.java

---
+++ Task 004/Main.java
@@ -0,0 +1,73 @@
+/**
+* Problem Statement:
+* Given an array of integers, determine if the array has duplicate elements. 
+*/
+
+import java.util.*;
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    System.out.println();
+
+    System.out.println("Test 1: Duplicates Present Testcase");
+    int numArray1[] = {5, 3, -1, 4, 5, 7, 9, -1};
+
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNSq( numArray1 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesN( numArray1 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNLogN( numArray1 ) );
+
+    System.out.println();
+
+    System.out.println("Test 2: No Duplicates Testcase");
+    int numArray2[] = {5, 3, -1, 4, 7, 9};
+
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNSq( numArray2 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesN( numArray2 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNLogN( numArray2 ) );
+
+    System.out.println();
+
+    System.out.println("Test 3: Single Element Testcase");
+    int numArray3[] = {5};
+
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNSq( numArray3 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesN( numArray3 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNLogN( numArray3 ) );
+
+    System.out.println();
+
+    System.out.println("Test 4: No Elements Testcase");
+    int numArray4[] = {};
+
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNSq( numArray4 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesN( numArray4 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNLogN( numArray4 ) );
+
+
+
+    System.out.println();
+    System.out.println("Test 5: Random Testcase");
+    
+    Random random = new Random();
+
+    int arrayLength = random.nextInt(10);
+    int numArray5[] = new int[arrayLength];
+
+    System.out.print("Printing the randomized array: ");
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+      numArray5[i] = random.nextInt(8 + 8) - 8;
+      System.out.print(numArray5[i] + " ");
+    }  
+
+    System.out.println();
+    System.out.println();
+
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNSq( numArray5 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesN( numArray5 ) );
+    System.out.println( DuplicateElementsSolution.containsDuplicatesNLogN( numArray5 ) );
+  }
+}
