f367308b9ca8f4c81316ae53a28aab221cb643e3
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-14 21:25:33 +0530

    Update Main.java

--- Task 001/Main.java
+++ Task 001/Main.java
@@ -9,34 +9,22 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println();
-
-    System.out.println("Test 1: Natural number testcase");
+    System.out.println("\nTest 1: Natural number testcase");
     FactorsOfNumberSolution.factorsSqrtN(24);
 
-    System.out.println();
-
-    System.out.println("Test 2: Natural number testcase");
+    System.out.println("\nTest 2: Natural number testcase");
     FactorsOfNumberSolution.factorsSqrtN(100);
 
-    System.out.println();
-
-    System.out.println("Test 3: Zero number testcase");
+    System.out.println("\nTest 3: Zero number testcase");
     FactorsOfNumberSolution.factorsSqrtN(0);
 
-    System.out.println();
-
-    System.out.println("Test 4: Fraction number testcase");
+    System.out.println("\nTest 4: Fraction number testcase");
     FactorsOfNumberSolution.factorsSqrtN(201/2);
 
-    System.out.println();
-
-    System.out.println("Test 5: Negative number testcase");
+    System.out.println("\nTest 5: Negative number testcase");
     FactorsOFNumberSolution.factorsSqrtN(-100);
 
-    System.out.println();
-
-    System.out.println("Test 6: Random testcase");
+    System.out.println("\nTest 6: Random testcase");
 
     Random random = new Random();
     int num = random.nextInt(1000 + 1000) - 1000;
