95e589f140a835b315aadc6fd28bb6dcf185e8c3
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-14 21:31:57 +0530

    Update Main.java

--- Task 003/Main.java
+++ Task 003/Main.java
@@ -9,57 +9,44 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println();
-
     int numArray1[] = {10, 4, 3, 50, 23, 90};
-    System.out.println("Test 1: Natural Numbers testcase");
+    System.out.println("\nTest 1: Natural Numbers testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray1);
     LargestThreeNumsSolution.findThreeLargestNums(numArray1);
 
-    System.out.println();
-
     int numArray2[] = {4,3,2,1};
-    System.out.println("Test 2: Natural Numbers in Descending Order Testcase");
+    System.out.println("\nTest 2: Natural Numbers in Descending Order Testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray2);
     LargestThreeNumsSolution.findThreeLargestNums(numArray2);
 
-    System.out.println();
-
     int numArray3[] = {10, 4, -3, 50, 23, -90};
-    System.out.println("Test 3: Negative & Positve Numbers Mixed testcase");
+    System.out.println("\nTest 3: Negative & Positve Numbers Mixed testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray3);
     LargestThreeNumsSolution.findThreeLargestNums(numArray3);
 
-    System.out.println();
-
     int numArray4[] = {-10, -4, -3, -50, -23, -90};
-    System.out.println("Test 4: All Negative Numbers testcase");
+    System.out.println("\nTest 4: All Negative Numbers testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray4);
     LargestThreeNumsSolution.findThreeLargestNums(numArray4);
 
-    System.out.println();
-
     int numArray5[] = {10, 4};
-    System.out.println("Test 5: Array Size Less Than 3 testcase");
+    System.out.println("\nTest 5: Array Size Less Than 3 testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray5);
     LargestThreeNumsSolution.findThreeLargestNums(numArray5);
 
-    System.out.println();
-
     int numArray6[] = {4, 4, 4};
-    System.out.println("Test 6: Identical Elements testcase");
+    System.out.println("\nTest 6: Identical Elements testcase");
 
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray6);
     LargestThreeNumsSolution.findThreeLargestNums(numArray6);
 
 
-    System.out.println();
-    System.out.println("Test 7: Random testcase");
+    System.out.println("\nTest 7: Random testcase");
 
     Random random = new Random();
 
@@ -74,9 +61,7 @@ class Main
       System.out.print(numArray7[i] + " ");
     }  
 
-    System.out.println();
-    System.out.println();
-
+    System.out.println("\n");
     LargestThreeNumsSolution.findThreeLargestNumsOptimal(numArray7);
     LargestThreeNumsSolution.findThreeLargestNums(numArray7);
   }
