438e07c5cbc63958630d506cc4956b018a8dfcea
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-16 15:05:45 +0530

    Update Main.java

--- Task 005/Main.java
+++ Task 005/Main.java
@@ -10,51 +10,44 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println();
-    System.out.println("Test 1: True Testcase");
+    System.out.println("\nTest 1: True Testcase");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("bob'sdeli", "bhaanobsdzel'yia") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("bob'sdeli", "bhaanobsdzel'yia") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("bob'sdeli", "bhaanobsdzel'yia") );
 
-    System.out.println();
-    System.out.println("Test 2: False Testcase");
+    System.out.println("\nTest 2: False Testcase");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("bob'sdeli", "ohaanbdzelyi") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("bob'sdeli", "ohaanbdzelyi") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("bob'sdeli", "ohaanbdzelyi") );
 
-    System.out.println();
-    System.out.println("Test 3: Empty Sign Testcase");
+    System.out.println("\nTest 3: Empty Sign Testcase");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("", "ohaanbdzelyi") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("", "ohaanbdzelyi") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("", "ohaanbdzelyi") );
 
-    System.out.println();
-    System.out.println("Test 4: Empty Given String Testcase");
+    System.out.println("\nTest 4: Empty Given String Testcase");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("bob'sdeli", "") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("bob'sdeli", "") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("bob'sdeli", "") );
 
-    System.out.println();
-    System.out.println("Test 5: Both Empty String Testcase");
+    System.out.println("\nTest 5: Both Empty String Testcase");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("", "") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("", "") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("", "") );
 
-    System.out.println();
-    System.out.println("Test 6: Sign String Longer than Given String");
+    System.out.println("\nTest 6: Sign String Longer than Given String");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM("bobby'sstunningdeli", "bhobnbgndel'yria") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN("bobby'sstunningdeli", "bhobnbgndel'yria") );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN("bobby'sstunningdeli", "bhobnbgndel'yria") );
 
 
-    System.out.println();
-    System.out.println("Test 7: Random Testcase");
+    System.out.println("\nTest 7: Random Testcase");
 
     Random random = new Random();
     
@@ -70,17 +63,14 @@ class Main
     for (int i = 0; i < givenCharsLength; i++)
       givenChars += (char) (random.nextInt(123 - 97) + 97);
 
-    System.out.println("Printing the input strings: Sign String = " + sign + "  Given String = " + givenChars);
-
-    System.out.println();
+    System.out.println("Printing the input strings: Sign String = " + sign + "  Given String = " + givenChars + "\n");
 
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNM(sign, givenChars) );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedN(sign, givenChars) );
     System.out.println( GenerateSignFromStrSolution.canSignBeGeneratedNLogN(sign, givenChars) );
 
 
-    System.out.println();
-    System.out.println("Test 8: Random Testcase");
+    System.out.println("\nTest 8: Random Testcase");
 
     String signStrList[] = {"bob'smarket", "storeofbob", "bob'sworldofgoods", "bob'sshop", "thebobshop"};
 
@@ -89,10 +79,8 @@ class Main
     int signStrListIndex = random.nextInt(5); 
     int givenStrListIndex = random.nextInt(5);
 
-    System.out.println("Printing the strings: Sign String = " + signStrList[ signStrListIndex ] + " Given String = " + givenStrList[ givenStrListIndex ]);
+    System.out.println("Printing the strings: Sign String = " + signStrList[ signStrListIndex ] + " Given String = " + givenStrList[ givenStrListIndex ] + "\n");
 
-    System.out.println();
-    
     String signStr = signStrList[ signStrListIndex ];
     String givenStr = givenStrList[ givenStrListIndex ];
 
