a12c5fdc73f0d730c40e1c7b756afbf26e316dbf
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-17 21:48:14 +0530

    Update Main.java

--- Task 006/Main.java
+++ Task 006/Main.java
@@ -11,33 +11,23 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println();
-
-    System.out.println("Test 1");
+    System.out.println("\nTest 1");
     ShiftEveryLetterByKSolution.shiftEveryLetterByKN("hello world", 4);
     ShiftEveryLetterByKSolution.shiftEveryLetterByKUsingMap("hello world", 4);
 
-    System.out.println();
-
-    System.out.println("Test 2");
+    System.out.println("\nTest 2");
     ShiftEveryLetterByKSolution.shiftEveryLetterByKN("hello, world!", 6);
     ShiftEveryLetterByKSolution.shiftEveryLetterByKUsingMap("hello, world!", 6);
 
-    System.out.println();
-
-    System.out.println("Test 3: Negative value of k testcase");
+    System.out.println("\nTest 3: Negative value of k testcase");
     ShiftEveryLetterByKSolution.shiftEveryLetterByKN("hello world", -4);
     ShiftEveryLetterByKSolution.shiftEveryLetterByKUsingMap("hello world", -4);
 
-    System.out.println();
-
-    System.out.println("Test 4: Zero value of k testcase");
+    System.out.println("\nTest 4: Zero value of k testcase");
     ShiftEveryLetterByKSolution.shiftEveryLetterByKN("hello world", 0);
     ShiftEveryLetterByKSolution.shiftEveryLetterByKUsingMap("hello world", 0);
 
-    System.out.println();
-
-    System.out.println("Test 5: Random testcase");
+    System.out.println("\nTest 5: Random testcase");
     
     Random random = new Random();
 
@@ -49,10 +39,8 @@ class Main
     for (int i = 0; i < strLen; i++)
       inputStr += (char) (random.nextInt(126 - 94) + 94);
 
-    System.out.println("Printing the input: \nString = " + inputStr + " \nk = " + k);
+    System.out.println("Printing the input: \nString = " + inputStr + " \nk = " + k + "\n");
 
-    System.out.println();
-    
     ShiftEveryLetterByKSolution.shiftEveryLetterByKN(inputStr, k);
     ShiftEveryLetterByKSolution.shiftEveryLetterByKUsingMap(inputStr, k);  
   }
