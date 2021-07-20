b086cab7fbe9a6759a44e4d78d7110866d980647
sahanaranga <52922491+sahanaranga@users.noreply.github.com>
2021-07-20 18:51:46 +0530

    Update Main.java

--- Task 009/Main.java
+++ Task 009/Main.java
@@ -10,9 +10,7 @@ class Main
 {
   public static void main(String[] args) 
   {
-    System.out.println();
-
-    System.out.println("Test 1");
+    System.out.println("\nTest 1");
     int numArrayA1[] = {-1, 5, 15, 23, 29, 5};
     int numArrayB1[] = {34, 17, 28, 36, 9};
 
@@ -21,31 +19,27 @@ class Main
 
     System.out.println();
 
-    System.out.println("Test 2: ");
+    System.out.println("\nTest 2: ");
     int numArrayA2[] = {1, 60, -10, 70, -90, 84};
     int numArrayB2[] = {86, 3, -15, 53, -80, 8};
     
     PairWithMinDiffSolution.findPairWithMinDiff( numArrayA2, numArrayB2 );
     PairWithMinDiffSolution.findPairWithMinDiffSort( numArrayA2, numArrayB2 );
 
-    System.out.println();
-
     /** 
     *It is found in this testcase that when multiple pairs have the same absolute minimum difference, 
     * the two algorithms provide different answers as they search for the pairs in different ways. 
     * If method "findPairWithMinDiff" uses a sorting algorithm, then the answers will be the same for both algorithms
     */
-    System.out.println("Test 3: Multiple Pairs testcase");
+    System.out.println("\nTest 3: Multiple Pairs testcase");
     int numArrayA3[] = {4, -7, -2};
     int numArrayB3[] = {-1, 7, 9, 3};
     
     PairWithMinDiffSolution.findPairWithMinDiff( numArrayA3, numArrayB3 );
     PairWithMinDiffSolution.findPairWithMinDiffSort( numArrayA3, numArrayB3 );
 
-    System.out.println();
-
     
-    System.out.println("Test 3: Random testcase");
+    System.out.println("\nTest 3: Random testcase");
 
     Random random = new Random();
 
@@ -71,9 +65,8 @@ class Main
       System.out.print(numArrayB4[i] + " ");
     } 
 
-    System.out.println();
+    System.out.println("\n");
 
-    System.out.println();
     PairWithMinDiffSolution.findPairWithMinDiff( numArrayA4, numArrayB4 );
     PairWithMinDiffSolution.findPairWithMinDiffSort( numArrayA4, numArrayB4 );
   }
